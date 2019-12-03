d9c18660c89c80ee418d1a39144b070177e99b07
jman11111 <jbroncato@yahoo.com>
2019-12-03 13:50:35 -0600

    SideBar WORKS

--- FirstApp/App.js
+++ FirstApp/App.js
@@ -33,15 +33,34 @@ class MasterView extends React.Component{
 
   AppCallback = (Shape) => {
     this.setState({shapes: Shape})
-    canvasShapes.push(Shape);
+    if(Shape == "triangle"){
+      var canvShape = {
+        key: getID(),
+        type: Shape,
+        height: 100,
+        width: 50,
+        zind: 0
+      }
+    }else{
+      var canvShape = {
+        key: getID(),
+        type: Shape,
+        height: 100,
+        width: 100,
+        zind: 0
+      }
+    }
+    canvasShapes.push(canvShape);
   }
 
   render(){
     return(
       <SafeAreaView style={styles.container}>
-        <ScrollViewButtons onChange={this.AppCallback} onCanvasPress={this.props.onCanvasPress}></ScrollViewButtons>
+        <ScrollViewButtons onChange={this.AppCallback}></ScrollViewButtons>
         <View style={styles.View}>
-          {canvasShapes}
+          {canvasShapes.map(shape => (
+            <CanvasShape zind={shape.zind} usekey={shape.key} key={shape.key} currwidth={shape.width} currheight={shape.height} renderShape={shape.type} onPress={this.props.onCanvasPress}/>
+          ))}
         </View>
         
       </SafeAreaView>
@@ -63,10 +82,10 @@ class ScrollViewButtons extends React.Component{
   render(){
     return(
       <ScrollView contentContainerStyle={styles.Scrollelements} style={styles.scrollView} horizontal={true} decelerationRate={0} snapToInterval={200} snapToAlignment={"center"}>
-        <SquareScrollButton onPress={this.ScrollButtonCallback} renderOnPress={<CanvasShape renderShape={"square"} onPress={this.props.onCanvasPress}/>}></SquareScrollButton>
-        <TriangleScrollButton onPress={this.ScrollButtonCallback} renderOnPress={<CanvasShape renderShape={"triangle"} onPress={this.props.onCanvasPress}/>}></TriangleScrollButton>
-        <CircleScrollButton onPress={this.ScrollButtonCallback} renderOnPress={<CanvasShape renderShape={"circle"} onPress={this.props.onCanvasPress}/>}></CircleScrollButton>
-        <SquareScrollButton onPress={this.ScrollButtonCallback} renderOnPress={<CanvasShape renderShape={"square"} onPress={this.props.onCanvasPress}/>}></SquareScrollButton>
+        <SquareScrollButton onPress={this.ScrollButtonCallback} renderOnPress={"square"}></SquareScrollButton>
+        <TriangleScrollButton onPress={this.ScrollButtonCallback} renderOnPress={"triangle"}></TriangleScrollButton>
+        <CircleScrollButton onPress={this.ScrollButtonCallback} renderOnPress={"circle"}></CircleScrollButton>
+        <SquareScrollButton onPress={this.ScrollButtonCallback} renderOnPress={"square"}></SquareScrollButton>
       </ScrollView>
     );
   }
@@ -133,15 +152,16 @@ class SettingsSideBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false
+      open: false,
+      currshapeID: 0
     };
   }
 
-  toggleOpen = () => {
-    this.setState({ open: !this.state.open });
-    alert("pressed");
+  toggleOpen = (sentID) => {
+    this.setState({ open: !this.state.open, currshapeID: sentID});
+    alert(sentID);
   };
-
+  // search array for shape with currshapeID, then change its CSS
   drawerContent = () => {
     return (
       <View style={{backgroundColor:"red", flex:1}}>
@@ -151,17 +171,32 @@ class SettingsSideBar extends React.Component {
         <Text style={{marginTop: 100, marginLeft:10}}>Height:</Text>
         <TextInput
           style={styles.TextInputShape}
-          onChangeText={text => onChangeText()}
+          onSubmitEditing = {(event) => {
+            for(i=0;i<canvasShapes.length;i++){
+              let changeHeight = canvasShapes.find(shape => shape.key == this.state.currshapeID);
+              changeHeight.height = parseInt(event.nativeEvent.text);
+            }
+          }}
         />
         <Text style={{marginTop: 100, marginLeft:10}}>Width:</Text>
         <TextInput
           style={styles.TextInputShape}
-          onChangeText={text => onChangeText()}
+          onSubmitEditing = {(event) => {
+            for(i=0;i<canvasShapes.length;i++){
+              let changeWidth = canvasShapes.find(shape => shape.key == this.state.currshapeID);
+              changeWidth.width = parseInt(event.nativeEvent.text);
+            }
+          }}
         />
          <Text style={{marginTop: 100, marginLeft:10}}>Layer:</Text>
         <TextInput
           style={styles.TextInputShape}
-          onChangeText={text => onChangeText()}
+          onSubmitEditing = {(event) => {
+            for(i=0;i<canvasShapes.length;i++){
+              let changeLayer = canvasShapes.find(shape => shape.key == this.state.currshapeID);
+              changeLayer.zind = parseInt(event.nativeEvent.text);
+            }
+          }}
         />
       </View>
     );
@@ -189,53 +224,54 @@ class SettingsSideBar extends React.Component {
 class CanvasShape extends React.Component{
   constructor(props) {
     super(props);
-
     this.state = {
-      pan: new Animated.ValueXY()
+      pan: new Animated.ValueXY(),
     };
-  }
-
-  componentDidMount() {
-    this.props.key = getID();
   }
 
   getShape = () => {
   if(this.props.renderShape == 'circle') {
     return{
-      width: 100,
-      height: 100,
+      position: "absolute",
+      width: this.props.currwidth,
+      height: this.props.currheight,
       borderRadius: 100/2,
       backgroundColor: 'blue',
-      margin: 3
+      margin: 3,
+      zIndex: this.props.zind
     };
   }else if(this.props.renderShape == 'square') {
     return{
-      width: 100,
-      height: 100,
+      position: "absolute",
+      width: this.props.currwidth,
+      height: this.props.currheight,
       backgroundColor: 'green',
       padding: 12,
-      margin: 3 
+      margin: 3,
+      zIndex: this.props.zind
     };
   }else if(this.props.renderShape == 'triangle') {
     return{
+      position: "absolute",
       width: 0,
       height: 0,
       backgroundColor: 'transparent',
       borderStyle: 'solid',
-      borderLeftWidth: 50,
-      borderRightWidth: 50,
-      borderBottomWidth: 100,
+      borderLeftWidth: this.props.currwidth,
+      borderRightWidth: this.props.currwidth,
+      borderBottomWidth: this.props.currheight,
       borderLeftColor: 'transparent',
       borderRightColor: 'transparent',
       borderBottomColor: 'red',
-      margin: 3
+      margin: 3,
+      zIndex: this.props.zind
     };
   }
 }
 
   render(){
     return(
-      <Draggable pressDrag={this.props.onPress} reverse={false} renderShape={'button'}>
+      <Draggable pressDrag={() => {this.props.onPress(this.props.usekey)}} reverse={false} renderShape={'button'}>
         <Text style={this.getShape()}/>
       </Draggable>  
     );
